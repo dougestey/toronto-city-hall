a5ad4c3fda2caee419afc8bc300b14a4181646a1
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-22 14:30:55 -0800

    Try different webapp format.

--- webapp.js
+++ webapp.js
@@ -4,5 +4,5 @@ var path    = require('path');
 /********************* APP SETUP *****************************/
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'platforms/browser/www')));
+app.use(express.static(path.join(__dirname, 'www')));
 app.listen(process.env.PORT || 8000);
