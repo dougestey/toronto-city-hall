17a5c061f22b7e8c887083c367fae82a7d60060d
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-21 17:11:09 -0800

    Group motions by date.

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('hall', ['ui.router', 'ngAria', 'ngAnimate', 'ngMaterial', 'hall.controllers', 'hall.services'])
+angular.module('hall', ['ui.router', 'ngAria', 'ngAnimate', 'ngMaterial', 'hall.controllers', 'hall.services', 'angular.filter'])
 
 .constant('apiUrl', 'https://tocityhall-api.herokuapp.com/v0')
 
