8b8c98b52590dc65dce8f90c70ba1ec405435355
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-09 14:32:25 -0800

    Added apiUrl constant for councillor and ward controllers.

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,4 +1,5 @@
 angular.module('hall.constants', [])
   .constant('apiUrlProd', 'https://tocityhall-api.herokuapp.com/v0')
+  .constant('apiUrl', 'https://tocityhall-api-staging.herokuapp.com/v0')
   .constant('apiUrlStaging', 'https://tocityhall-api-staging.herokuapp.com/v0')
   .constant('apiUrlLocal', 'http://localhost/v0')
